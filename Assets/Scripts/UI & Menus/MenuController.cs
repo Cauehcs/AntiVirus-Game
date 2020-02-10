2a289a5acb243caa6d5bf4ef325870e095a810a5
Cauehcs <caue.hcs@gmail.com>
2020-02-10 06:42:05 -0300

    preparation for the flow of screens

---
+++ Assets/Scripts/UI & Menus/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] string currentScene, nextScene;
+
+    private void Awake()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    private void Start()
+    {
+        if(currentScene == "Menu")
+        {
+            StartCoroutine(Timer(0));
+        }
+    }
+
+    IEnumerator Timer(int TimerUse)
+    {
+        switch (TimerUse)
+        {
+            case 0:
+                    yield return new WaitForSeconds(8);
+                break;
+        }        
+    }
+
+}
