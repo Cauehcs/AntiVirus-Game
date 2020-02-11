9ad28336a9236a3331516598775d1c8e5da91d87
Cauehcs <caue.hcs@gmail.com>
2020-02-11 06:34:28 -0300

    Home menu added

--- Assets/Scripts/UI & Menus/MenuController.cs
+++ Assets/Scripts/UI & Menus/MenuController.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] string currentScene, nextScene;
+    [SerializeField]bool canGoMenu, canBackMenu;
+    int indexMenu;
+
+    [SerializeField] private GameObject[] canvasMenu;
 
     private void Awake()
     {
@@ -20,12 +24,55 @@ public class MenuController : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        StartMenuBehaviour();
+    }
+
+    void StartMenuBehaviour()
+    {
+        if (canGoMenu)
+        {
+            print("CanGoMenu Actived!");
+            if (Input.anyKey && !Input.GetKey(KeyCode.Escape))
+            {
+                canvasMenu[1].SetActive(true);
+                canBackMenu = true;
+            }
+        }
+
+        if (canBackMenu)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                canvasMenu[1].GetComponent<Animator>().speed = -1;
+                canBackMenu = false;
+            }
+        }   
+    }
+
+    public void GoFaseOneButton()
+    {
+        SceneManager.LoadScene("Fase One");
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
     IEnumerator Timer(int TimerUse)
     {
         switch (TimerUse)
         {
             case 0:
                     yield return new WaitForSeconds(8);
+                    canGoMenu = true;
                 break;
         }        
     }
